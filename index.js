cbab51194cf8a9f5bb81072a9aeaa96b6dcc38cb
MohdBasurra <mohdye96@gmail.com>
2022-12-25 00:05:20 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(num1 , num2){
+   let sum= num1 + num2;
+    return(sum)
+}
+
+function subtract(num1 , num2){
+    let sum= num1 - num2;
+     return(sum)
+ }
+
+ 
+ function multiply(num1 , num2){
+    let sum= num1 * num2;
+     return(sum)
+ }
+
+ function divide(num1 , num2){
+    let sum= num1 / num2;
+     return(sum)
+ }
+
+ function increment(n){
+    n++
+    return n
+}
+function decrement(n){
+    n--
+    return n
+}
+function makeInt(n){
+    return parseInt(n,10)
+}
+function preserveDecimal(n){
+    return parseFloat (n)
+}
